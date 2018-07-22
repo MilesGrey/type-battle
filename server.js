8c2a1aef702f77d72962f6f7717553dd2e4c8589
Benjamin Jin <benjaminmjin@gmail.com>
2018-07-23 00:56:31 +0200

    add socket io server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const server = express();
+const http = require('http').Server(server);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 8080;
+const HOST = 'localhost';
+
+server.use(express.static(__dirname + '/public'))
+
+io.on('connection', (socket) => {
+  console.log('a user connected!');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected.');
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server is listenting on http://${HOST, PORT}`);
+})
