95fbe67d5e41427be917622c5907113a7ee73d26
Benjamin Jin <benjaminmjin@gmail.com>
2018-07-25 03:09:07 +0200

    implement game rooms

--- client/components/MyDesk.js
+++ client/components/MyDesk.js
@@ -3,36 +3,76 @@ import openSocket from 'socket.io-client';
 import Words from './Words';
 import WordInput from './WordInput';
 
-const socket = openSocket('http://localhost:8080');
+const socket = openSocket();
 
 export default class MyDesk extends React.Component {
   state = {
-    words: ['lamp', 'dog', 'component']
+    gameId: undefined,
+    words: []
   }
 
-  componentDidMount() {
-    socket.on('wordCompleted', word => {
-      console.log(word);
-      
-      this.setState((prevState) => ({
-        words: prevState.words.concat(word)
+  componentWillMount() {
+    socket.on('error', data => {
+      console.log(data.message);
+    });
+
+    socket.on('initializeRoom', (data) => {
+      this.setState(() => ({
+        words: data.words,
+        gameId: data.gameId
       }));
+
+      socket.on('enemyWordCompleted', word => {
+        this.setState((prevState) => ({
+          words: prevState.words.concat(word)
+        }));
+      });
     });
   }
 
   handleWordSubmit = (inputWord) => {
     if (this.state.words.indexOf(inputWord) === -1 ) {
-      return true;
+      return;
     }
 
     this.setState((prevState) => ({
       words: prevState.words.filter(word => inputWord != word)
     }));
-    socket.emit('wordCompleted', inputWord);
+
+    const data = {
+      gameId: this.state.gameId,
+      word: inputWord
+    }
+    socket.emit('wordCompleted', data);
+  }
+
+  handleCreateGame = () => {
+    socket.emit('hostCreateNewGame');
+  }
+
+  handleJoinGame = (e) => {
+    e.preventDefault();
+
+    const gameId = e.target.elements.gameId.value.trim();
+    socket.emit('playerJoinGame', gameId);
   }
 
   render = () => (
     <div>
+      {
+        !this.state.gameId &&
+        <button onClick={this.handleCreateGame}>Create Game</button>
+      }
+      {
+        !this.state.gameId &&
+        <form onSubmit={this.handleJoinGame}>
+          <input placeholder='game id' type='text' name='gameId' />
+          <button>join game</button>
+        </form>
+      }
+      {
+        !!this.state.gameId && <p>{ this.state.gameId }</p>
+      }
       <Words
         words={this.state.words}
         handleWordSubmit={this.handleWordSubmit}
